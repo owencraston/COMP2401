7ef7f4a4996a5760960c2bb8ce21613bedd0f8e6
Owen Craston <owen.craston@shopify.com>
2017-11-09 21:23:56 -0500

    fix summary function and menu/quit function

--- a3/assignment3/uni.c
+++ a3/assignment3/uni.c
@@ -11,21 +11,18 @@
 
 void summary(struct person p[], int arraySize);
 
-int main()
-{
+int main() {
     struct person person[NUM_RECORDS];
-
 
     populateRecords(person, NUM_RECORDS);
 
     //switch statement for users options
     int choice;
-    unsigned int running = 0;
+    unsigned int running = 1;
 
-    while (running == 0) {
+    while (running == 1) {
         //get the value from the menu function
         choice = menu();
-
         switch(choice){
                 case 1:            
                 // call the printAllEmployees function
@@ -45,6 +42,7 @@ int main()
                 break;          
             case 0:
                 running = quit();
+                break;
         }
     }
         
@@ -80,85 +78,74 @@ int menu() {
     return choice;
 }
 
-
-void summary(struct person p[], int arraySize) {
+void summary(struct person person[], int size) {
     //create variables to store the summary values
     int numRecords = 0;
+    //student info
     int numStudents = 0;
     int totalGpa = 0;
     int totalNumCourses = 0;
     int totalTuition = 0;
+    //employee info
     int numEmployees = 0;
     int minLevel = 15; //set at max
     int maxLevel = 0; //set at min
     int totalYOS = 0;
     int totalSalary = 0;
 
+  for (int i=0; i<size; i++) {
+      // increment the number of records
+    numRecords++;
+    // check if the person is a student
+    if (person[i].emplyeeOrStudent == 1) {
+        // add to the studet counter
+      numStudents++;
 
-    //iterate throught the array and get the values
-    for (int i=0; i<arraySize; i++) {
-        //increment the numRecord var
-        numRecords ++;
-        // check if the person is a student
-        if (p[i].emplyeeOrStudent == 1) {
-            //calculate all the student related attributes
-            //increment the number of students
-            numStudents ++;
-            //gpa
-            totalGpa += p[i].student.gpa;
-            //average number of courses
-            totalNumCourses += p[i].student.numCourses;
-            //average tuition
-            totalTuition += p[i].student.tuitionFees;
-        } else if (p[i].emplyeeOrStudent == 0) {
-            //calculate all the values related to the employee
-            //increment the number of employees
-            numEmployees ++;
-            // check if this is employee is the max level
-            if (p[i].employee.level > maxLevel) {
-                //set this value as the new max
-                maxLevel = p[i].employee.level;
-            }
-            //check if this employee is the min level
-            if (p[i].employee.level < minLevel) {
-                //set this value as the new min level
-                minLevel = p[i].employee.level;
-            }
-            //increment the years of service
-            totalYOS += p[i].employee.yearsService;
-            //increment the salary
-            totalSalary += p[i].employee.salary;
-        }
+      //add to the student values based on the current student
+      totalGpa += person[i].student.gpa;
+      totalNumCourses += person[i].student.numCourses;
+      totalTuition += person[i].student.tuitionFees;
     }
+    else {
+      numEmployees++;
+      totalYOS += person[i].employee.yearsService;
+      totalSalary += person[i].employee.salary;
 
-    //calculate averages
-    //students
-    float avgGpa = avgGpa/numStudents;
-    float avgNumCourses = avgNumCourses/numStudents;
-    float avgTuition = avgTuition/numStudents;
-    //employee
-    float avgYOS = avgYOS/numEmployees;
-    float avgSalary = avgSalary/numEmployees;
+      // check for min/max level
+      if (person[i].employee.level > maxLevel) maxLevel = person[i].employee.level; // set max if value is greater than current max
+      if (person[i].employee.level < minLevel) minLevel = person[i].employee.level; //set min if value us smaller than current min
+    }
+  }
+
+    // claculate avergaes
+    float avgGpa = totalGpa/numStudents;
+    float avgNumCourses = totalNumCourses/numStudents;
+    float avgTuition = totalTuition/numStudents;
+
+    float avgSalary = totalSalary/numEmployees;
+    float avgYOS = totalYOS/numEmployees;
+
 
     //print the results
     printf("Output:\n");
     printf("Total number of records: %d\n", numRecords);
-	printf("Student Stats:\n");
-	printf("Number of Students: %d\n", numStudents);
-	printf("Average GPA:  %.2f", avgGpa);
-	printf(", Average Number of courses:  %.2f", avgNumCourses);
+    printf("Student Stats:\n");
+    printf("Number of Students: %d\n", numStudents);
+    printf("Average GPA:  %.2f", avgGpa);
+    printf(", Average Number of courses:  %.2f", avgNumCourses);
     printf(", Average Tutition Fees: %.2f\n", avgTuition);
     // employee
     printf("Employees' Stats:\n");
-	printf("Number of Employees: %d\n", numEmployees);
-	printf("Max Level: %d", maxLevel);
-	printf(", Min Level: %d\n", minLevel);
-	printf("Average Years of Service:  %.2f", avgYOS);
-	printf(", Average Salary:  %.2f\n\n", avgSalary);
+    printf("Number of Employees: %d\n", numEmployees);
+    printf("Max Level: %d", maxLevel);
+    printf(", Min Level: %d\n", minLevel);
+    printf("Average Years of Service:  %.2f", avgYOS);
+    printf(", Average Salary:  %.2f\n\n", avgSalary);
+
 }
 
 int quit() {
-    char yn[3]; //make var to store user input
+    char yn[2]; //make var to store user input
     printf("are you sure you want to quit the program? Type y/n to proceed\n");
     scanf("%s", &yn); //get input
 
