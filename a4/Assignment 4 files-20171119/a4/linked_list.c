c5e305a70ee09ac5f7b71ad73bea68e4ddcf5832
Owen Craston <owen.craston@shopify.com>
2017-11-19 19:55:48 -0500

    start a4

---
+++ a4/Assignment 4 files-20171119/a4/linked_list.c
@@ -0,0 +1,391 @@
+
+/* 
+File name is linked_list.c
+ Purpose: file contains functions for manipulating singly linked list
+ 
+ Created By
+ Doron Nussbaum 2016
+ 
+ Modifications:
+ November 2017 - minor modifications
+ 
+ 
+ Copyright 2017
+ 
+ */
+
+/******************************************************************/
+// INCLUDE 
+
+#include "stdio.h"
+#include "stdlib.h"
+#include "string.h"
+#include "linked_list.h"
+
+
+/************************************************************************/
+// Define 
+
+
+
+/************************************************************************/
+
+/*
+Purpose: insert a new node into the list as the first element
+input
+id - id of person
+firstName - first name of person
+familyName - family name of person
+
+input/output
+head - head of linked list
+
+return
+A pointer to the node that was allocated.  
+
+NULL - if the operation was not successful
+
+*/
+
+PersonalInfo *insertToList(PersonalInfo **head, unsigned int id, 
+        char *firstName, char *familyName)
+{
+	// add code
+    
+
+}
+
+
+/************************************************************************/
+/*
+Purpose: insert a new node into the list after the given node  
+
+Input
+node - the node after which the new node must be added to the list
+id - id of person
+firstName - first name of person
+familyName - family name of person
+
+
+return
+A pointer to the node that was allocated.  
+
+NULL - if the operation was not successful
+
+*/
+
+
+PersonalInfo *insertAfter(PersonalInfo *node, unsigned int id, char *firstName, char *familyName)
+{
+	// add code 
+
+
+}
+
+/************************************************************************/
+/*
+Purpose: create a new node and insert it into the end of the list
+Input
+head - the head of the list
+id - id of person
+firstName - first name of person
+familyName - family name of person
+
+
+return
+A pointer to the node that was allocated.  
+
+NULL - if the operation was not successful
+
+*/
+
+
+PersonalInfo *insertLast(PersonalInfo **head, unsigned int id, char *firstName, char *familyName)
+{
+	// add code
+
+}
+
+
+/************************************************************************/
+/*
+Purpose: search for the first node with the matching firstName
+Input
+head - the head of the list
+firstName - first name of person
+
+return
+a pointer to the node that was found.
+NULL - if no node was found or list empty
+
+*/
+
+
+PersonalInfo *searchByName(PersonalInfo *head, char *firstName)
+{
+	// add code 
+
+}
+
+/************************************************************************/
+/*
+Purpose: search for the first node with the matching id
+Input
+head - the head of the list
+id - id of person person
+
+return
+a pointer to the node that was allocated.  
+
+NULL - if no node was found or list empty 
+
+*/
+
+
+PersonalInfo *searchById(PersonalInfo *head, unsigned int id)
+{
+	// add code 
+    
+}
+
+/***************************************************************/
+
+/*
+Purpose: delete the first node from the list
+Input
+head - the head of the list
+
+output
+head - the head of the list
+firstName - first name of delted record
+familyName - family name of deleted recrod
+id - id of deleted record
+
+
+return
+
+0 if node was deleted
+1 if node was not deleted or list is empty
+
+*/
+
+
+int deleteFromList(PersonalInfo **head, unsigned int *id,
+	char *firstName, char *familyName)
+
+{
+
+    // add code
+
+}
+
+
+/***************************************************************/
+
+/*
+Purpose: delete the last node from the list
+Input
+head - the head of the list
+
+output
+head - the head of the list
+firstName - first name of delted record
+familyName - family name of deleted recrod
+id - id of deleted record
+
+
+return
+
+0 if node was deleted  
+1 if node was not deleted or list is empty 
+
+*/
+
+
+int deleteLast(PersonalInfo **head, unsigned int *id,
+                char *firstName, char *familyName)
+
+{
+
+    // add code 
+
+}
+
+
+
+/************************************************************************/
+
+/*
+Purpose: delete the record after the given node 
+Input
+node - a node in the list
+
+output
+firstName - first name of delted record
+familyName - family name of deleted recrod
+id - id of deleted record
+
+
+return
+0 if node was deleted 
+1 if node was not deleted (either input node is NULL or input node was the lastnode in the list)
+
+*/
+
+
+
+int deleteAfter(PersonalInfo *node, unsigned int *id,
+                char *firstName, char *familyName)
+
+{
+    // add code 
+
+}
+
+/************************************************************************/
+
+/*
+Purpose: delete the first node with the matching firstName
+Input
+head - the head of the list
+firstName - first name of person
+
+output
+head - the head of the list
+firstName - first name of delted record
+familyName - family name of deleted recrod
+id - id of deleted record
+
+
+return
+0  if node was deleted
+1 if node was not found (e.g., list is empty, no such node exists)
+
+*/
+
+
+int deleteNodeByName(PersonalInfo **head, char *firstName,
+	char *familyName, unsigned int *id)
+{
+	// add code 
+
+
+}
+/************************************************************************/
+/*
+Purpose: deletes all nodes from the list
+input/output
+head - the head of the list
+
+
+*/
+
+
+void deleteList(PersonalInfo **head)
+{
+	// add code 
+
+
+}
+
+
+
+/************************************************************************/
+/*
+Purpose: prints the fields of a node
+input:
+node - a pointer to a given node
+
+*/
+
+
+void printNode(PersonalInfo *node)
+{
+	printf("%-20s %-20s %7d \n",node->firstName, node->familyName, node->id);
+
+}
+
+
+/************************************************************************/
+/*
+Purpose: prints all the records in the list
+
+input
+head - the head of the list
+*/
+
+
+void printList(PersonalInfo *head)
+{
+	// add code 
+
+
+
+}
+
+/************************************************************************/
+/*
+Purpose: counts the number of nodes in the list
+input
+head - the head of the list
+
+return 
+the number of nodes in the list
+
+*/
+
+
+int listSize(PersonalInfo *head)
+{
+	// add code 
+
+
+
+}
+
+/************************************************************************/
+/*
+Purpose: counts the number of nodes in the list with a matching firstName
+input
+head - the head of the list
+
+return 
+the number of nodes in the list that match the firstName
+
+
+*/
+
+
+int countRecords(PersonalInfo *head, char *firstName)
+{
+	// add code 
+
+
+}
+
+
+
+
+/************************************************************************/
+/*
+
+Purpose: removes all duplicates records from the list.  Duplicate records are determined by their first name. 
+You can assume that the listed is sorted by first name.
+
+input
+head - the head of the  list
+
+
+
+*/
+
+
+
+
+void removeDuplicates(PersonalInfo *head)
+{
+	// add code 
+
+
+}
+
